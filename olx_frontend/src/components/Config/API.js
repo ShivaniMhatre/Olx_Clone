7aeb09bba3dacab7c0b64d128b114099d1301fe5
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-01-18 11:34:34 +0530

    updates

--- olx_frontend/src/components/Config/API.js
+++ olx_frontend/src/components/Config/API.js
@@ -12,8 +12,8 @@
 
 const api = process.env.NODE_ENV != 'development' ?
     process.env.REACT_APP_BASE_URL :
-    'http://localhost:7000'
-    // 'https://becchoapp.onrender.com'
+    // 'http://localhost:7000'
+    'https://becchoapp.onrender.com'
 
 console.log(process.env, "API_URL");
 
